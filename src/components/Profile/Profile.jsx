1dbc8ae025346c4b55632db4036c8d6ca808c1b0
Igor <105238566+IgorZhyvotkov@users.noreply.github.com>
2023-05-15 19:35:26 +0300

    asd

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,31 @@
 export const Profile =  ({username, tag, location, avatar, stats: {label, quantity}} ) => {
-    <div class="profile">
-  <div class="description">
-    <img
-      src={avatar}
-      alt="User avatar"
-      className="avatar"
-    />
-    <p className="name">{username}</p>
-    <p className="tag">{tag}</p>
-    <p className="location">{location}</p>
-  </div>
-
-  <ul className="stats">
-    <li>
-      <span className="label">{label}</span>
-      <span className="quantity">{quantity}</span>
-    </li>
-    <li>
-      <span className="label">{label}</span>
-      <span className="quantity">{quantity}</span>
-    </li>
-    <li>
-      <span className="label">{label}</span>
-      <span className="quantity">{quantity}</span>
-    </li>
-  </ul>
-</div>
+    return(
+        <div class="profile">
+        <div class="description">
+          <img
+            src={avatar}
+            alt="User avatar"
+            className="avatar"
+          />
+          <p className="name">{username}</p>
+          <p className="tag">{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+      
+        <ul className="stats">
+          <li>
+            <span className="label">{label}</span>
+            <span className="quantity">{quantity}</span>
+          </li>
+          <li>
+            <span className="label">{label}</span>
+            <span className="quantity">{quantity}</span>
+          </li>
+          <li>
+            <span className="label">{label}</span>
+            <span className="quantity">{quantity}</span>
+          </li>
+        </ul>
+      </div>
+    );
 };
